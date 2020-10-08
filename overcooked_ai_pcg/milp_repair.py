16ae1be3cf30950dc078aecb22be35dc6144cbff
Bryon Tjanaka <38124174+btjanaka@users.noreply.github.com>
2020-10-08 13:16:18 -0700

    Ray Memory Fix (#4)
    * Autoformatter
    
    * Use context manager for MILP repair

--- overcooked_ai_pcg/milp_repair.py
+++ overcooked_ai_pcg/milp_repair.py
@@ -1,7 +1,8 @@
-from overcooked_ai_pcg.helper import lvl_str2number, lvl_number2str, obj_types
-from overcooked_ai_py import read_layout_dict
 import numpy as np
 from docplex.mp.model import Model
+from overcooked_ai_py import read_layout_dict
+
+from overcooked_ai_pcg.helper import lvl_number2str, lvl_str2number, obj_types
 
 
 def read_in_fix_demo_layout():
@@ -32,7 +33,8 @@ def add_object_placement(mdl, all_objects):
 #
 # post condition: these constraints ensure that a path exists from some source
 #                 object to all sink objects
-def add_reachability(mdl, graph, source_objects, sink_objects, blocking_objects, cnt):
+def add_reachability(mdl, graph, source_objects, sink_objects, blocking_objects,
+                     cnt):
     # Transpose the blocking objects matrix so all blocking objects for
     # a given node are easily accessible.
     blocking = list(zip(*blocking_objects))
@@ -46,7 +48,9 @@ def add_reachability(mdl, graph, source_objects, sink_objects, blocking_objects,
     rev = [[] for i in range(n_nodes)]
     for i, neighbors in enumerate(graph):
         for j in neighbors:
-            f = mdl.integer_var(name='p_{}_{}-{}'.format(i, j, cnt), lb=0, ub=n_nodes)
+            f = mdl.integer_var(name='p_{}_{}-{}'.format(i, j, cnt),
+                                lb=0,
+                                ub=n_nodes)
             flow[i].append(f)
             rev[j].append(f)
 
@@ -60,7 +64,8 @@ def add_reachability(mdl, graph, source_objects, sink_objects, blocking_objects,
         demands.append(f)
     # Add a flow conservation constraint for each node (outflow == inflow)
     for i in range(n_nodes):
-        mdl.add_constraint(supplies[i] + sum(rev[i]) == demands[i] + sum(flow[i]))
+        mdl.add_constraint(supplies[i] + sum(rev[i]) == demands[i] +
+                           sum(flow[i]))
 
     # Add capacity constraints for each edge ensuring that no flow passes through a blocking object
     for i, neighbors in enumerate(flow):
@@ -98,7 +103,8 @@ def add_edit_distance(mdl, graph, objects, add_movement=True):
                     costs.append(cost_change * cur_var)
 
     else:
-        for obj_id, (objects_in_graph, cost_move, cost_change) in enumerate(objects):
+        for obj_id, (objects_in_graph, cost_move,
+                     cost_change) in enumerate(objects):
 
             # Setup a flow network for each edge in the graph
             n_nodes = len(graph)
@@ -109,7 +115,10 @@ def add_edit_distance(mdl, graph, objects, add_movement=True):
             rev = [[] for i in range(n_nodes)]
             for i, neighbors in enumerate(graph):
                 for j in neighbors:
-                    f = mdl.integer_var(name='edit({})_{}_{}'.format(obj_id, i, j), lb=0, ub=n_nodes)
+                    f = mdl.integer_var(name='edit({})_{}_{}'.format(
+                        obj_id, i, j),
+                                        lb=0,
+                                        ub=n_nodes)
                     costs.append(cost_move * f)
                     flow[i].append(f)
                     rev[j].append(f)
@@ -120,25 +129,32 @@ def add_edit_distance(mdl, graph, objects, add_movement=True):
             waste = []
             num_supply = 0
             for i, (cur_var, did_contain) in enumerate(objects_in_graph):
-                f = mdl.integer_var(name='edit({})_{}_t'.format(obj_id, i), lb=0, ub=1)
+                f = mdl.integer_var(name='edit({})_{}_t'.format(obj_id, i),
+                                    lb=0,
+                                    ub=1)
                 demands.append(f)
 
                 # Add a second sink that eats any flow that doesn't find a home.
                 # The cost of this flow is deleting the object.
-                f = mdl.integer_var(name='edit({})_{}_t2'.format(obj_id, i), lb=0, ub=n_nodes)
+                f = mdl.integer_var(name='edit({})_{}_t2'.format(obj_id, i),
+                                    lb=0,
+                                    ub=n_nodes)
                 costs.append(cost_change * f)
                 waste.append(f)
 
                 # Flow conservation constraint (inflow == outflow)
                 if did_contain:
                     # If we had a piece of this type in the current node, match it to the outflow
-                    mdl.add_constraint(1 + sum(rev[i]) == demands[i] + sum(flow[i]) + waste[i])
+                    mdl.add_constraint(1 + sum(rev[i]) == demands[i] +
+                                       sum(flow[i]) + waste[i])
                     num_supply += 1
                 else:
-                    mdl.add_constraint(sum(rev[i]) == demands[i] + sum(flow[i]) + waste[i])
+                    mdl.add_constraint(
+                        sum(rev[i]) == demands[i] + sum(flow[i]) + waste[i])
 
             # Ensure we place a piece of this type to match it to the demand.
-            for (cur_var, did_contain), node_demand in zip(objects_in_graph, demands):
+            for (cur_var,
+                 did_contain), node_demand in zip(objects_in_graph, demands):
                 mdl.add_constraint(node_demand <= cur_var)
 
             # Ensure that the source and sink have the same flow.
@@ -146,11 +162,16 @@ def add_edit_distance(mdl, graph, objects, add_movement=True):
 
     mdl.minimize(mdl.sum(costs))
 
-def add_reachability_helper(source_labels, sink_labels, blocking_labels, mdl, adj, objs, cnt):
+
+def add_reachability_helper(source_labels, sink_labels, blocking_labels, mdl,
+                            adj, objs, cnt):
     source_objects = [objs[obj_types.index(label)] for label in source_labels]
     sink_objects = [objs[obj_types.index(label)] for label in sink_labels]
-    blocking_objects = [objs[obj_types.index(label)] for label in blocking_labels]
-    add_reachability(mdl, adj, source_objects, sink_objects, blocking_objects, cnt)
+    blocking_objects = [
+        objs[obj_types.index(label)] for label in blocking_labels
+    ]
+    add_reachability(mdl, adj, source_objects, sink_objects, blocking_objects,
+                     cnt)
 
 
 def repair_lvl(np_lvl):
@@ -179,82 +200,86 @@ def repair_lvl(np_lvl):
         if is_border:
             border_nodes.append(i)
 
-    new_level = []
-    mdl = Model()
+    with Model() as mdl:
 
-    objs = []
-    for obj_label in obj_types:
-        curr_type = [mdl.integer_var(name='obj_{}_{}'.format(obj_label, i), lb=0, ub=1) for i in range(n_nodes)]
-        objs.append(curr_type)
+        objs = []
+        for obj_label in obj_types:
+            curr_type = [
+                mdl.integer_var(name='obj_{}_{}'.format(obj_label, i),
+                                lb=0,
+                                ub=1) for i in range(n_nodes)
+            ]
+            objs.append(curr_type)
 
-    # ensure one cell contains one obj_type
-    add_object_placement(mdl, objs)
+        # ensure one cell contains one obj_type
+        add_object_placement(mdl, objs)
 
-    # Ensure that all cells on the boundary are walls
-    not_allowed_on_border = []
-    for label in "12 ":
-        i = obj_types.index(label)
-        not_allowed_on_border += [objs[i][j] for j in border_nodes]
-    mdl.add_constraint(sum(not_allowed_on_border) <= 0)
+        # Ensure that all cells on the boundary are walls
+        not_allowed_on_border = []
+        for label in "12 ":
+            i = obj_types.index(label)
+            not_allowed_on_border += [objs[i][j] for j in border_nodes]
+        mdl.add_constraint(sum(not_allowed_on_border) <= 0)
 
-    # Player1 and 2 show up exactly once
-    mdl.add_constraint(sum(objs[obj_types.index("1")]) == 1)
-    mdl.add_constraint(sum(objs[obj_types.index("2")]) == 1)
+        # Player1 and 2 show up exactly once
+        mdl.add_constraint(sum(objs[obj_types.index("1")]) == 1)
+        mdl.add_constraint(sum(objs[obj_types.index("2")]) == 1)
 
-    # At least one onion, dish plate, pot, and serve point
-    mdl.add_constraint(sum(objs[obj_types.index("O")]) >= 1)
-    mdl.add_constraint(sum(objs[obj_types.index("D")]) >= 1)
-    mdl.add_constraint(sum(objs[obj_types.index("P")]) >= 1)
-    mdl.add_constraint(sum(objs[obj_types.index("S")]) >= 1)
+        # At least one onion, dish plate, pot, and serve point
+        mdl.add_constraint(sum(objs[obj_types.index("O")]) >= 1)
+        mdl.add_constraint(sum(objs[obj_types.index("D")]) >= 1)
+        mdl.add_constraint(sum(objs[obj_types.index("P")]) >= 1)
+        mdl.add_constraint(sum(objs[obj_types.index("S")]) >= 1)
 
-    # Upper bound number of onion, dish plate, pot, and serve point
-    mdl.add_constraint(sum(objs[obj_types.index("O")]) <= 2)
-    mdl.add_constraint(sum(objs[obj_types.index("D")]) <= 2)
-    mdl.add_constraint(sum(objs[obj_types.index("P")]) <= 2)
-    mdl.add_constraint(sum(objs[obj_types.index("S")]) <= 4)
+        # Upper bound number of onion, dish plate, pot, and serve point
+        mdl.add_constraint(sum(objs[obj_types.index("O")]) <= 2)
+        mdl.add_constraint(sum(objs[obj_types.index("D")]) <= 2)
+        mdl.add_constraint(sum(objs[obj_types.index("P")]) <= 2)
+        mdl.add_constraint(sum(objs[obj_types.index("S")]) <= 4)
 
-    # reachability
-    source_labels = "1"
-    sink_labels = "ODPS "
-    blocking_labels = "XODPS2"
-    add_reachability_helper(source_labels, sink_labels, blocking_labels, mdl, adj, objs, 0)
+        # reachability
+        source_labels = "1"
+        sink_labels = "ODPS "
+        blocking_labels = "XODPS2"
+        add_reachability_helper(source_labels, sink_labels, blocking_labels,
+                                mdl, adj, objs, 0)
 
-    source_labels = "2"
-    sink_labels = "ODPS "
-    blocking_labels = "XODPS1"
-    add_reachability_helper(source_labels, sink_labels, blocking_labels, mdl, adj, objs, 1)
+        source_labels = "2"
+        sink_labels = "ODPS "
+        blocking_labels = "XODPS1"
+        add_reachability_helper(source_labels, sink_labels, blocking_labels,
+                                mdl, adj, objs, 1)
 
-    # add edit distance objective
-    objects = []
-    cost_move = 1
-    cost_change = 20
-    for cur_idx, cur_obj in enumerate(objs):
-        objects_in_graph = []
+        # add edit distance objective
+        objects = []
+        cost_move = 1
+        cost_change = 20
+        for cur_idx, cur_obj in enumerate(objs):
+            objects_in_graph = []
+            for r in range(n):
+                for c in range(m):
+                    i = r * m + c
+                    objects_in_graph.append((cur_obj[i], cur_idx == np_lvl[r,
+                                                                           c]))
+            objects.append((objects_in_graph, cost_move, cost_change))
+
+        add_edit_distance(mdl, adj, objects)
+
+        solution = mdl.solve()
+
+        def get_idx_from_variables(solution, node_id):
+            for i, obj_var in enumerate(objs):
+                if solution.get_value(obj_var[node_id]) == 1:
+                    return i
+            return -1
+
+        # Extract the new level from the milp model
+        new_lvl = np.zeros((n, m))
         for r in range(n):
             for c in range(m):
                 i = r * m + c
-                objects_in_graph.append((cur_obj[i], cur_idx == np_lvl[r,c]))
-        objects.append((objects_in_graph, cost_move, cost_change))
-
-    add_edit_distance(mdl, adj, objects)
-
-    solution = mdl.solve()
-
-
-    def get_idx_from_variables(solution, node_id):
-        for i, obj_var in enumerate(objs):
-            if solution.get_value(obj_var[node_id]) == 1:
-                return i
-        return -1
-
-    # Extract the new level from the milp model
-    new_lvl = np.zeros((n, m))
-    for r in range(n):
-        for c in range(m):
-            i = r * m + c
-            new_lvl[r,c] = get_idx_from_variables(solution, i)
-    return new_lvl.astype(np.uint8)
-
+                new_lvl[r, c] = get_idx_from_variables(solution, i)
+        return new_lvl.astype(np.uint8)
 
 
 def main():
@@ -264,6 +289,7 @@ def main():
     print(before_fix)
     after_fix = lvl_number2str(repair_lvl(np_lvl).astype(np.uint8))
     print(after_fix)
+
 
 if __name__ == "__main__":
     main()
