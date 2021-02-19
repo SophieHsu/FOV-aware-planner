remove erroneous normalization

overcooked_ai_py/mdp/overcooked_mdp.py
@@ -390,10 +390,10 @@ class OvercookedState(object):
 
     def cal_concurrent_active_sum(self, ):
         concurrent_active_log = self.cal_concurrent_active_log()
-        #res = np.sum(concurrent_active_log)
-        res = 0
-        if len(concurrent_active_log) > 0:
-            res = round(np.sum(concurrent_active_log)/len(concurrent_active_log)*100)
+        res = np.sum(concurrent_active_log)
+        # res = 0
+        # if len(concurrent_active_log) > 0:
+        #     res = round(np.sum(concurrent_active_log)/len(concurrent_active_log)*100)
 
         return res
 
@@ -415,10 +415,10 @@ class OvercookedState(object):
 
     def cal_total_stuck_time(self, ):
         stuck_logs = self.get_player_stuck_log()
-        #res = sum(stuck_logs[0])
-        res = 0
-        if len(stuck_logs[0]) > 0:
-            res = round(sum(stuck_logs[0])/len(stuck_logs[0])*100)
+        res = sum(stuck_logs[0])
+        # res = 0
+        # if len(stuck_logs[0]) > 0:
+        #     res = round(sum(stuck_logs[0])/len(stuck_logs[0])*100)
         return res
 
     def get_player_stuck_log(self, ):