b8c934479cdafa1cda710f591a669ea0cfceb300
Yulun Zhang <yulunzha@usc.edu>
2021-07-05 15:53:25 -0700

    demo garage env

--- overcooked_ai_rl/tests/test_gym_env.py
+++ overcooked_ai_rl/tests/test_gym_env.py
@@ -6,6 +6,12 @@ from overcooked_ai_py.mdp.overcooked_env import OvercookedEnv, OvercookedV1
 from overcooked_ai_py.agents.agent import *
 from overcooked_ai_rl.featurize_fn import *
 
+from garage import wrap_experiment
+from garage.torch.policies import CategoricalCNNPolicy
+from garage.torch.algos import PPO
+from garage.torch.optimizers import OptimizerWrapper
+from garage.envs import GymEnv
+
 
 def render_level(layout_name):
     _, _, env = setup_env(layout_name)
@@ -13,7 +19,7 @@ def render_level(layout_name):
     env.render()
     time.sleep(60)
 
-# Green hat is agent 1/robot; Blue hat is agent2/human
+# Green hat is agent 1/robot; Blue hat is agent 2/human
 def setup_env(layout_name):
     mdp = OvercookedGridworld.from_layout_name(layout_name)
     env = OvercookedEnv.from_mdp(mdp, info_level = 0, horizon=100)
@@ -33,6 +39,17 @@ overcooked_v1 = OvercookedV1(agent1, agent2, env, lossless_state_featurize)
 print(overcooked_v1.observation_space)
 print(overcooked_v1.action_space)
 
+env = GymEnv(overcooked_v1)
+
+policy = CategoricalCNNPolicy(
+    env.spec,
+    image_format="NHWC",
+    kernel_sizes=(5, 3, 3),
+    hidden_channels=(25, 25, 25),
+)
+
+print(policy)
+
 # done = False
 # while not done:
 #     env.render()
@@ -45,7 +62,7 @@ print(overcooked_v1.action_space)
 done = False
 next_obs = overcooked_v1.reset()
 while not done:
-    env.render()
-    ai_action = np.random.randn(6)
+    env.render(mode="human")
+    ai_action, _ = policy.get_action(torch.Tensor(next_obs))
     next_obs, reward, done, info = overcooked_v1.step(ai_action)
     time.sleep(0.1)
